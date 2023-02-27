7ac45b34817e9efc778ac534eb42fd3b031d1633
ＨｅｎｄＭｕｎ <88487729+muniroh96@users.noreply.github.com>
2023-02-27 12:05:47 +0700

    Add files via upload

---
+++ FILEX.py
@@ -0,0 +1,11 @@
+import os
+print('give star to our repo')
+os.system('git pull && clear')
+
+print('update finished\nJoin Our Facebook Group and support us')
+os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
+
+
+os.system('clear')
+
+import XXV
